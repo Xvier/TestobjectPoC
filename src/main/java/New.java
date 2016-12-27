90beda04fda8e6f8441092dc4d91fc0e3b461512
Xvier <xviuda@xebia.com>
2016-12-27 16:12:05 +0100

    Added jacoco to build

---
+++ src/main/java/New.java
@@ -0,0 +1,15 @@
+
+public class New {
+
+
+
+    public static void main(String[] args) {
+        callJava();
+    }
+
+    private static void callJava() {
+        System.out.println("demotest");
+    }
+
+
+}
--- src/test/java/StartTest.java
+++ src/test/java/StartTest.java
@@ -4,12 +4,10 @@ import org.junit.runner.RunWith;
 import org.testobject.appium.junit.TestObjectAppiumSuite;
 import org.testobject.rest.api.appium.common.TestObject;
 
-/**
- * Created by xviuda on 22-12-16.
- */
+
 
 @RunWith(TestObjectAppiumSuite.class)
-@TestObject(testLocally = false, testObjectSuiteId = 7, testObjectApiKey = "CB1DF80B672F4D248D37E7C9AF466A9F")
+@TestObject(testObjectSuiteId = 7, testObjectApiKey = "CB1DF80B672F4D248D37E7C9AF466A9F")
 
 public class StartTest extends AbstractTest {
 
@@ -20,13 +18,6 @@ public class StartTest extends AbstractTest {
         app.loginScreen().acceptConditions();
         app.tvScreen().checkSearch();
     }
-
-    @Test
-    public void secondTest(){
-        System.out.println("do something2222");
-    }
-
-
 
 
 }
